b909e135758bc443ee2bcc3b8f4933d81cf81e72
kataray <katarinamantay@gmail.com>
2025-05-03 12:41:39 -0400

    Sidebar Changes

--- src/components/Navbar/Sidebar.tsx
+++ src/components/Navbar/Sidebar.tsx
@@ -1,8 +1,49 @@
 function Sidebar() {
     return (
-        <section className="bg-[#19191c] relative w-70 h-screen overflow-hidden">
+        <section className="bg-[#19191c] relative w-90 h-screen overflow-hidden">
 
+            <h1 className="font-cal text-5xl flex justify-left ml-5 mt-5 text-[#FFFFFF]">Planack</h1>
+
+            <div className="mt-10 space-y-6 ml-5 flex flex-col">
+
+                <div className="sidebar-link hover:bg-[#3c3c3c] hover:border-b-2 hover:border-[#FFFFFF] hover:scale-105 hover:rounded-md transition-all duration-200 p-3">
+                    <h2 className="font-cal text-xl text-white">Dashboard</h2>
+                </div>
+
+                <div className="sidebar-link hover:bg-[#3c3c3c] hover:border-b-2 hover:border-[#FFFFFF] hover:scale-105 hover:rounded-md transition-all duration-200 p-3">
+                    <h2 className="font-cal text-xl text-white">Calendar</h2>
+                </div>
+
+                <div className="sidebar-link hover:bg-[#3c3c3c] hover:border-b-2 hover:border-[#FFFFFF] hover:scale-105 hover:rounded-md transition-all duration-200 p-3">
+                    <h2 className="font-cal text-xl text-white">Team</h2>
+                </div>
+
+                <div className="sidebar-link hover:bg-[#3c3c3c] hover:border-b-2 hover:border-[#FFFFFF] hover:scale-105 hover:rounded-md transition-all duration-200 p-3">
+                    <h2 className="font-cal text-xl text-white">Tasks</h2>
+                </div>
+
+                <div className="sidebar-link hover:bg-[#3c3c3c] hover:border-b-2 hover:border-[#FFFFFF] hover:scale-105 hover:rounded-md transition-all duration-200 p-3">
+                    <h2 className="font-cal text-xl text-white">Resources</h2>
+                </div>
+
+                <div className="sidebar-link hover:bg-[#3c3c3c] hover:border-b-2 hover:border-[#FFFFFF] hover:scale-105 hover:rounded-md transition-all duration-200 p-3">
+                    <h2 className="font-cal text-xl text-white">Finance</h2>
+                </div>
+
+                <div className="sidebar-link hover:bg-[#3c3c3c] hover:border-b-2 hover:border-[#FFFFFF] hover:scale-105 hover:rounded-md transition-all duration-200 p-3">
+                    <h2 className="font-cal text-xl text-white">Workshops</h2>
+                </div>
+
+                <div className="sidebar-link hover:bg-[#3c3c3c] hover:border-b-2 hover:border-[#FFFFFF] hover:scale-105 hover:rounded-md transition-all duration-200 p-3">
+                    <h2 className="font-cal text-xl text-white">Timeline</h2>
+                </div>
+
+                <div className="sidebar-link hover:bg-[#3c3c3c] hover:border-b-2 hover:border-[#FFFFFF] hover:scale-105 hover:rounded-md transition-all duration-200 p-3">
+                    <h2 className="font-cal text-xl text-white">Photos</h2>
+                </div>
+            </div>
         </section>
     );
 }
+
 export default Sidebar;
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                madimi: ['"Madimi One"', ...defaultTheme.fontFamily.sans],
+                marmelad: ['"Marmelad"', ...defaultTheme.fontFamily.serif],
+                magra: ['"Magra"', ...defaultTheme.fontFamily.serif],
+            }
+        }
+    },
+}
