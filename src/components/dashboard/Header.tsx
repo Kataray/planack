af554fff0ae690ea891cce087860c282a13134e4
kataray <katarinamantay@gmail.com>
2025-05-03 13:41:07 -0400

    Edit to dashboard

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -1,4 +1,5 @@
 import Sidebar from 'src/components/Navbar/Sidebar.tsx';
+import CardDemo from '@/components/ui/cards-demo-2.tsx'
 
 function Header() {
     return (
@@ -6,6 +7,10 @@ function Header() {
             <Sidebar />
             <section className="bg-[#000000] relative w-screen h-screen overflow-hidden">
 
+                <h1 className="font-cal text-[4vh] flex justify-start ml-10 mt-7 text-[#FFFFFF]"> Hackathon Dashboard </h1>
+                <h1 className="font-cal text-[2.5vh] flex justify-start ml-10 -mt-2 text-[#FFFFFF]"> Every hacks on track </h1>
+
+                <CardDemo />
             </section>
         </div>
     );
