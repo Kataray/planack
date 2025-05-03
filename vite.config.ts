f0c2bb2722e10a1abf682a028b41161664f7b3a7
kataray <katarinamantay@gmail.com>
2025-05-03 10:24:04 -0400

    added tailwind and shadcn to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 export default defineConfig({
-  plugins: [react()],
-})
+
+  plugins: [
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
